068b82544bf6c20fac96f92ee644ee0fb9956e93
Hammed Amusat Oriyomi <mederhoo@gmail.com>
2024-05-09 10:53:46 +0100

    mysql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
--- A SQL script select band_name, and lifespan column which is difference
+-- This SQL script select band_name, and lifespan column which is difference
 SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
     FROM metal_bands
     WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
