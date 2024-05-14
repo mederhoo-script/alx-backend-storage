04ebb5b5e9fd12991e4f5ccc59fe53b4eca7f1e4
Hammed Amusat Oriyomi <mederhoo@gmail.com>
2024-05-14 20:38:40 +0100

    nosql

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
 """
-List all documents in Python
+a script that lists all databases in MongoDB.
 """
 import pymongo
+from pymongo import MongoClient
 
 
 def list_all(mongo_collection):
     """
-    function to list all doc in a collection
+    script that lists all databases in MongoDB.
     """
     if not mongo_collection:
         return []
     documents = mongo_collection.find()
     return [post for post in documents]
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    school_collection = client.my_db.school
+    schools = list_all(school_collection)
+    for school in schools:
+        print("[{}] {}".format(school.get('_id'), school.get('name')))
