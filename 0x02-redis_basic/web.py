e32357de87ae7e2292a249a17383be143e958ba1
Hammed Amusat Oriyomi <mederhoo@gmail.com>
2024-05-16 00:09:32 +0100

    redis

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,36 @@
 #!/usr/bin/env python3
 """
-Web cache and tracker
+web cache and tracker
 """
 import requests
 import redis
 from functools import wraps
 
-cache = redis.Redis()
+store = redis.Redis()
 
 
-def track_url_access(method):
-    """Decorator that counts how many times
-    a URL is accessed and caches the result"""
+def count_url_access(method):
+    """ Decorator counting how many times
+    a URL is accessed """
     @wraps(method)
     def wrapper(url):
-        cache_key = "cache:" + url
-        cached_response = cache.get(cache_key)
-        if cached_response:
-            return cached_response.decode("utf-8")
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
 
-        access_count_key = "access_count:" + url
-        html_content = method(url)
+        count_key = "count:" + url
+        html = method(url)
 
-        cache.incr(access_count_key)
-        cache.set(cache_key, html_content)
-        cache.expire(cache_key, 10)
-        return html_content
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
     return wrapper
 
 
-@track_url_access
+@count_url_access
 def get_page(url: str) -> str:
-    """Fetches and returns the HTML content of a URL"""
-    response = requests.get(url)
-    return response.text
+    """ Returns HTML content of a url """
+    res = requests.get(url)
+    return res.text
